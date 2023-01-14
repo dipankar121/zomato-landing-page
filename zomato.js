b002bd5fbf6adbfa92aa9a3d046659f3c91220be
dipankar121 <94380559+dipankar121@users.noreply.github.com>
2023-01-14 12:38:08 +0530

    first commit

---
+++ zomato.js
@@ -0,0 +1,9 @@
+const btnPlayStore = document.getElementById("google-play");
+const btnAppStore = document.getElementById("apple-store");
+
+btnPlayStore.addEventListener("click", () => {
+  alert("Clicked PlayStore");
+});
+btnAppStore.addEventListener("click", () => {
+  alert("Clicked AppStore");
+});
